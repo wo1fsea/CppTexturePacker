0e3e1abdb7d6b1daf6f29c96184206f76d106330
wo1fsea <wo1fsea@126.com>
2020-04-08 21:06:24 +0800

    add: ci

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
-cd ./build
-cmake -DCMAKE_TOOLCHAIN_FILE="c:/vcpkg/scripts/buildsystems/vcpkg.cmake" ../
-:-DVCPKG_TARGET_TRIPLET=x64-windows ../
+rmdir /s /q .\build
+mkdir build
+cd .\build
+cmake -DCMAKE_TOOLCHAIN_FILE="e:/vcpkg/scripts/buildsystems/vcpkg.cmake" ..\
+cmake --build .
+ctest -C Debug
 pause
