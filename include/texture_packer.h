c1a3c3952a9654850843797f885a1582f9a5e4b6
wo1fsea <wo1fsea@126.com>
2020-05-28 20:30:12 +0800

    add: pack

--- include/texture_packer.h
+++ include/texture_packer.h
@@ -54,14 +54,12 @@ namespace CppTexturePacker
         {
         }
         
-        void pack(const std::vector<std::string>& image_paths,
+        void pack(const std::vector<ImageInfo>& image_infos,
             const std::string & output_name,
-            const std::string & output_path="",
-            const std::string & input_base_path="")
+            const std::string & output_path,
+            const std::string & base_image_path="",
+            const std::string & image_format="png")
         {
-
-            auto image_infos = load_image_infos_from_paths(image_paths);
-
             if (trim_mode)
             {
                 for (auto image_info: image_infos)
@@ -83,21 +81,51 @@ namespace CppTexturePacker
             
             add_image_rects(image_rects);
             auto image_info_map = make_image_info_map(image_infos);
+            
+            assert((atlases.size() <=1 || output_name.find("%d") != std::string::npos));
 
-            for (auto atlas: atlases)
+            fs::path fp_out = output_path;
+
+            for (int i=0; i < atlases.size(); ++i)
             {
+                auto atlas = atlases[i];
+                std::string atlas_name = output_name;
+                if (output_name.find("%d") != std::string::npos)
+                {
+                    atlas_name = (boost::format(output_name) % i).str();
+                }
+                std::string image_file_name = atlas_name + "." + image_format;
+                fs::path image_path = fp_out / image_file_name;
+                fs::path plist_path = fp_out / (atlas_name + ".plist");
                 auto image = dump_altalas_to_image(atlas, image_info_map);
-                
-                // save_image_to_file(save_path, image);
-
+                if (reduce_border_artifacts)
+                {
+                    alpha_bleeding(image);
+                }
+                save_image_to_file(image_path.string(), image);
+                dump_altalas_to_plist(plist_path.string(), atlas, image_info_map, image_file_name, base_image_path);
             }
+        }
 
+        
+        void pack(const std::string& images_dir,
+            const std::string & output_name,
+            const std::string & output_path,
+            const std::string & base_image_path="",
+            const std::string & image_format="png")
+        {
+            auto image_infos = load_image_infos_from_dir(images_dir);
+            return pack(image_infos, output_name, output_path, base_image_path, image_format);
+        }
 
-
-            if (reduce_border_artifacts)
-            {
-
-            }
+        void pack(const std::vector<std::string>& image_paths,
+            const std::string & output_name,
+            const std::string & output_path,
+            const std::string & base_image_path="",
+            const std::string & image_format="png")
+        {
+            auto image_infos = load_image_infos_from_paths(image_paths);
+            return pack(image_infos, output_name, output_path, base_image_path, image_format);
         }
     };
 } // namespace CppTexturePacker
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,12 @@
-/*
 #include "cpp_texture_packer.h"
 
 
 int main(int argc, char* argv[])
 {
-    std::string path = "C:/GITHUB/CppTexturePacker/data/";
-    std::string save_path = "C:/GITHUB/CppTexturePacker/p2.png";
-    std::string save_path2 = "C:/GITHUB/CppTexturePacker/p3.png";
-    auto image_infos = CppTexturePacker::load_image_infos_from_dir(path);
-
-    auto packer = CppTexturePacker::RectPacker();
-    std::vector<CppTexturePacker::ImageRect> image_rects;
-    for(auto image_info: image_infos)
-    {
-        image_rects.emplace_back(image_info.get_image_rect());
-    }
-    packer.add_image_rects(image_rects);
-
-    auto image = dump_altalas_to_image(packer.atlases[0], make_image_info_map(image_infos));
-
-    CppTexturePacker::save_image_to_file(save_path, image);
-
-    CppTexturePacker::trim_image(image, CppTexturePacker::get_bounding_box(image));
-    CppTexturePacker::save_image_to_file(save_path2, image);
-    return 0;
-}
-*/
-#include "plist/plist.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-
-#ifdef _MSC_VER
-#pragma warning(disable:4996)
-#endif
-
-
-int main(int argc, char *argv[])
-{
-    char *plist_xml = NULL;
-    uint32_t size_out = 0;
-    char *file_out = "e:/Projects/a.plist";
-
-    auto plist_root = plist_new_dict();
-    plist_dict_set_item(plist_root, "a", plist_new_string("av"));
-
-    plist_to_xml(plist_root, &plist_xml, &size_out);
-    if (!plist_xml)
-    {
-        printf("PList XML writing failed\n");
-        return 8;
-    }
-    else
-        printf("PList XML writing succeeded\n");
-
-    if (plist_xml)
-    {
-        FILE *oplist = NULL;
-        oplist = fopen(file_out, "wb");
-        fwrite(plist_xml, size_out, sizeof(char), oplist);
-        fclose(oplist);
-    }
-
-    free(plist_xml);
-    plist_free(plist_root);
-
-    //success
+    std::string path = "e:/Projects/CppTexturePacker/data/";
+    // std::string save_path = "C:/GITHUB/CppTexturePacker/p2.png";
+    // std::string save_path2 = "C:/GITHUB/CppTexturePacker/p3.png";
+    auto p = CppTexturePacker::TexturePacker();
+    p.pack(path, "CppTexturePacker", "e:/Projects/CppTexturePacker/", "e:/Projects/CppTexturePacker/");
     return 0;
 }
